4cc32089952ab17cf14093a55be936b70054b9dd
darwin-joel12 <guagchingadarwin14@gmail.com>
2026-04-20 20:49:08 -0500

    Ataque Fuerza Bruta - Burp Suite

--- Dockerfile
+++ Dockerfile
@@ -19,5 +19,6 @@ RUN sed -i 's|/var/www/html|/var/www/html/public|g' /etc/apache2/sites-available
 # 5. Instalar Composer y dependencias
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 RUN composer install --no-dev --optimize-autoloader
-
+# Antes del CMD, limpiar vistas y configurar
+RUN php artisan view:clear
 CMD ["apache2-foreground"]
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Importante añadir esto
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        //
+        // Esto fuerza a Laravel a generar links HTTPS en Render
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
